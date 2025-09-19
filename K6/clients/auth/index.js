a7f5dd1d7f331979ea9843fc733e43623b625abd
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-09-19 10:32:32 +0200

    Add workflows to test Github Action

--- K6/clients/auth/index.js
+++ K6/clients/auth/index.js
@@ -1,4 +1,3 @@
 export { SystemRegisterApiClient } from "./systemRegister.js"
-export { SystemUserApiClient } from "./systemUser.js"
 export { SystemUserRequestApiClient } from "./systemUserRequest.js"
 export { RegisterApiClient } from "./register.js"
