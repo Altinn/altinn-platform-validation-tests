53da23848f51b5524c40b3770c534b86b26595c4
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-09-25 14:34:38 +0200

    Add post consent test

--- K6/clients/auth/index.js
+++ K6/clients/auth/index.js
@@ -1,3 +1,4 @@
 export { SystemRegisterApiClient } from "./systemRegister.js"
 export { SystemUserRequestApiClient } from "./systemUserRequest.js"
 export { RegisterApiClient } from "./register.js"
+export { ConsentApiClient } from "./consent.js"
