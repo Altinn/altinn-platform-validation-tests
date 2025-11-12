774d33324f8879e37f3b35452f6fb40a3191b3d7
Dagfinn Olsen <dagfinno@gmail.com>
2025-11-12 09:55:55 +0100

    add possibility to authorize for direct delegation

--- K6/api/tests/auth/pdpAuthorize/pdpAuthorizeDagl.js
+++ K6/api/tests/auth/pdpAuthorize/pdpAuthorizeDagl.js
@@ -4,15 +4,34 @@ import { getClients, getTokenOpts, getActionLabelAndExpectedResponse } from './c
 export { setup } from './commonFunctions.js';
 import exec from 'k6/execution';
 
+const directDelegation = (__ENV.DIRECT_DELEGATION ?? 'false') === 'true';;
+
 // Labels for different actions
 const pdpAuthorizeLabel = "PDP Authorize";
+const pdpAuthorizeDirectDelegationLabel = "PDP Authorize Direct delegation";
 const pdpAuthorizeLabelDenyPermit = "PDP Authorize Deny";
+const pdpAuthorizeLabelDenyDirectDelegationPermit = "PDP Authorize Deny Direct delegation";
 const tokenGeneratorLabel = "Personal Token Generator";
 
-export const options = getOptions([pdpAuthorizeLabel, pdpAuthorizeLabelDenyPermit, tokenGeneratorLabel]);
-
 // resource with read/write for PRIV and DAGL
-const resource = "ttd-dialogporten-performance-test-02";
+const resource1 = "ttd-dialogporten-performance-test-02";
+// resource with read/write for DIRECT DELEGATION
+const resource2 = "perf_direct_access_resource";
+
+let labels = [pdpAuthorizeLabel, pdpAuthorizeLabelDenyPermit, tokenGeneratorLabel];
+let denyLabel = pdpAuthorizeLabelDenyPermit;
+let permitLabel = pdpAuthorizeLabel;
+let resource = resource1;
+
+// Override for direct delegation tests
+if (directDelegation) {
+  labels = [pdpAuthorizeDirectDelegationLabel, pdpAuthorizeLabelDenyDirectDelegationPermit, tokenGeneratorLabel];
+  denyLabel = pdpAuthorizeLabelDenyDirectDelegationPermit;
+  permitLabel = pdpAuthorizeDirectDelegationLabel;
+  resource = resource2;
+}
+
+export const options = getOptions(labels);
 
 /**
  * Main function executed by each VU.
@@ -21,7 +40,7 @@ export default function (testData) {
   const [pdpAuthorizeClient, tokenGenerator] = getClients();
   const party = getItemFromList(testData[exec.vu.idInTest - 1], __ENV.RANDOMIZE);
   tokenGenerator.setTokenGeneratorOptions(getTokenOpts(party.ssn));
-  const [action, label, expectedResponse] = getActionLabelAndExpectedResponse(pdpAuthorizeLabelDenyPermit, pdpAuthorizeLabel);
+  const [action, label, expectedResponse] = getActionLabelAndExpectedResponse( denyLabel, permitLabel);
   PdpAuthorizeDagl(
     pdpAuthorizeClient,
     party.ssn,
