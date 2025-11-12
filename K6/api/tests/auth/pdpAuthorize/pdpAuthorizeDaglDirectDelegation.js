0ed99780f152ca4447c40ef757eeadebe559143a
Dagfinn Olsen <dagfinno@gmail.com>
2025-11-12 11:24:39 +0100

    add test for direct delegastion

---
+++ K6/api/tests/auth/pdpAuthorize/pdpAuthorizeDaglDirectDelegation.js
@@ -0,0 +1,35 @@
+import { PdpAuthorizeDagl } from '../../../building_blocks/auth/pdpAuthorize/index.js';
+import { getItemFromList, getOptions } from '../../../../helpers.js';
+import { getClients, getTokenOpts, getActionLabelAndExpectedResponse } from './commonFunctions.js';
+export { setup } from './commonFunctions.js';
+import exec from 'k6/execution';
+
+// Labels for different actions
+const pdpAuthorizeLabel = "PDP Authorize direct delegation";
+const pdpAuthorizeLabelDenyPermit = "PDP Authorize Deny direct delegation";
+const tokenGeneratorLabel = "Personal Token Generator";
+
+export const options = getOptions([pdpAuthorizeLabel, pdpAuthorizeLabelDenyPermit, tokenGeneratorLabel]);
+
+// resource with read/write for PRIV and DAGL
+const resource = "perf_direct_access_resource";
+
+/**
+ * Main function executed by each VU.
+ */
+export default function (testData) {
+  const [pdpAuthorizeClient, tokenGenerator] = getClients();
+  const party = getItemFromList(testData[exec.vu.idInTest - 1], __ENV.RANDOMIZE);
+  tokenGenerator.setTokenGeneratorOptions(getTokenOpts(party.ssn));
+  const [action, label, expectedResponse] = getActionLabelAndExpectedResponse(pdpAuthorizeLabelDenyPermit, pdpAuthorizeLabel);
+  PdpAuthorizeDagl(
+    pdpAuthorizeClient,
+    party.ssn,
+    party.orgno,
+    resource,
+    action,
+    expectedResponse,
+    __ENV.AUTHORIZATION_SUBSCRIPTION_KEY,
+    label
+  );
+}
