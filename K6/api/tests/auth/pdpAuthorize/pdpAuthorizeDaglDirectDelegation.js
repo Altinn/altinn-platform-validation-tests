659420f53b6bde9cabaf061a4cb09c286e3575cf
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2025-11-12 13:16:13 +0100

    small tweaks (#59)

--- K6/api/tests/auth/pdpAuthorize/pdpAuthorizeDaglDirectDelegation.js
+++ K6/api/tests/auth/pdpAuthorize/pdpAuthorizeDaglDirectDelegation.js
@@ -1,8 +1,8 @@
-import { PdpAuthorizeDagl } from '../../../building_blocks/auth/pdpAuthorize/index.js';
-import { getItemFromList, getOptions } from '../../../../helpers.js';
-import { getClients, getTokenOpts, getActionLabelAndExpectedResponse } from './commonFunctions.js';
-export { setup } from './commonFunctions.js';
-import exec from 'k6/execution';
+import { PdpAuthorizeDagl } from "../../../building_blocks/auth/pdpAuthorize/index.js";
+import { getItemFromList, getOptions } from "../../../../helpers.js";
+import { getClients, getTokenOpts, getActionLabelAndExpectedResponse } from "./commonFunctions.js";
+export { setup } from "./commonFunctions.js";
+import exec from "k6/execution";
 
 // Labels for different actions
 const pdpAuthorizeLabel = "PDP Authorize direct delegation";
@@ -18,18 +18,18 @@ const resource = "perf_direct_access_resource";
  * Main function executed by each VU.
  */
 export default function (testData) {
-  const [pdpAuthorizeClient, tokenGenerator] = getClients();
-  const party = getItemFromList(testData[exec.vu.idInTest - 1], __ENV.RANDOMIZE);
-  tokenGenerator.setTokenGeneratorOptions(getTokenOpts(party.ssn));
-  const [action, label, expectedResponse] = getActionLabelAndExpectedResponse(pdpAuthorizeLabelDenyPermit, pdpAuthorizeLabel);
-  PdpAuthorizeDagl(
-    pdpAuthorizeClient,
-    party.ssn,
-    party.orgno,
-    resource,
-    action,
-    expectedResponse,
-    __ENV.AUTHORIZATION_SUBSCRIPTION_KEY,
-    label
-  );
+    const [pdpAuthorizeClient, tokenGenerator] = getClients();
+    const party = getItemFromList(testData[exec.vu.idInTest - 1], __ENV.RANDOMIZE);
+    tokenGenerator.setTokenGeneratorOptions(getTokenOpts(party.ssn));
+    const [action, label, expectedResponse] = getActionLabelAndExpectedResponse(pdpAuthorizeLabelDenyPermit, pdpAuthorizeLabel);
+    PdpAuthorizeDagl(
+        pdpAuthorizeClient,
+        party.ssn,
+        party.orgno,
+        resource,
+        action,
+        expectedResponse,
+        __ENV.AUTHORIZATION_SUBSCRIPTION_KEY,
+        label
+    );
 }
