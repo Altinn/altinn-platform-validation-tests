e9ebf83c065875e3cf9d819bb33c128eb3849538
Dagfinn Olsen <dagfinno@gmail.com>
2025-11-14 10:34:41 +0100

    Performance/roles (#66)
    * added test for get roles
    
    * test-dev for test
    
    * add the test as well
    
    * add the test to test options
    
    * Add 'get-roles' to the workflow steps
    
    * Change environment from test to dev in workflow
    
    * adding vus and duration as env vars
    
    * adding vus and duration as env vars
    
    * adding vus and duration as env vars
    
    * adding vus and duration as env vars
    
    * adding vus and duration as env vars
    
    * adding vus and duration as env vars
    
    * correcting schedule for roles
    
    * small refactor
    
    * Added TODO
    
    * small tweaks (#67)
    
    ---------
    
    Co-authored-by: Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>

--- K6/clients/auth/index.js
+++ K6/clients/auth/index.js
@@ -5,3 +5,4 @@ export { ConsentApiClient } from "./consent.js";
 export { AuthorizedPartiesClient } from "./authorizedParties.js";
 export { PdpAuthorizeClient } from "./pdpAuthorize.js";
 export { ConnectionsApiClient } from "./connections.js";
+export { RolesApiClient } from "./roles.js";
---
+++ K6/clients/auth/roles.js
@@ -0,0 +1,42 @@
+import http from "k6/http";
+
+class RolesApiClient {
+    /**
+     *
+     * @param {string} baseUrl e.g. https://platform.at22.altinn.cloud
+     */
+    constructor(
+        baseUrl
+    ) {
+        /**
+        *
+        * @property {string} BASE_PATH The path to the api without host information
+        */
+        this.BASE_PATH = "/accessmanagement/api/v1/meta/info/roles";
+        /**
+         * @property {string} FULL_PATH The path to the api including protocol, hostname, etc.
+         */
+        this.FULL_PATH = baseUrl + this.BASE_PATH;
+
+    }
+
+    /**
+    * Get Roles
+    * Docs TODO: This link does not work, nothing yet {@link https://docs.altinn.studio/nb/api/accessmanagement/resourceowneropenapi/#/Roles}
+    * @param {string} label
+    * @returns http.RefinedResponse
+    */
+    GetRoles(label) {
+        const url = new URL(`${this.FULL_PATH}`);
+        let nameTag = label ? label : url.toString();
+        const params = {
+            tags: { name: nameTag },
+            headers: {
+                "Content-type": "application/json",
+            },
+        };
+        return http.get(url.toString(), params);
+    }
+}
+
+export { RolesApiClient };
