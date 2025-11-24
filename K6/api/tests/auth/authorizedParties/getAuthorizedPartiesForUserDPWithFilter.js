c6363b2cdf26776c6b8aba5feb6d3b2f797ddeda
Dagfinn Olsen <dagfinno@gmail.com>
2025-11-24 10:44:34 +0100

    removed branch env settings

--- K6/api/tests/auth/authorizedParties/getAuthorizedPartiesForUserDPWithFilter.js
+++ K6/api/tests/auth/authorizedParties/getAuthorizedPartiesForUserDPWithFilter.js
@@ -4,8 +4,6 @@ import { GetAuthorizedParties } from "../../../building_blocks/auth/authorizedPa
 import { getClients } from "./commonFunctions.js";
 import { getItemFromList, getOptions, parseCsvData } from "../../../../helpers.js";
 
-const branch = __ENV.BRANCH || "main";
-
 const randomize = (__ENV.RANDOMIZE ?? "true") === "true";
 
 const label = "getAuthorizedPartiesForUserDPWithFilter";
@@ -36,6 +34,6 @@ export default function (data) {
 }
 
 export function setup() {
-  const res = http.get(`https://raw.githubusercontent.com/Altinn/altinn-platform-validation-tests/refs/heads/${branch}/K6/testdata/auth/party-avgivere-${__ENV.ENVIRONMENT}.csv`);
+  const res = http.get(`https://raw.githubusercontent.com/Altinn/altinn-platform-validation-tests/refs/heads/performace/authorized-parties-with-filters/K6/testdata/auth/party-avgivere-${__ENV.ENVIRONMENT}.csv`);
   return parseCsvData(res.body);
 }
