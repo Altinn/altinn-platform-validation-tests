a17014f177d2e372516e594bbd478f90d05a7469
Dagfinn Olsen <dagfinno@gmail.com>
2025-12-08 13:21:18 +0100

    added test for IncludePartiesViaKeyRole

---
+++ K6/api/tests/auth/authorizedParties/getAuthorizedPartiesForUserIncludePartiesViaKeyRoles.js
@@ -0,0 +1,28 @@
+
+import { GetAuthorizedParties } from "../../../building_blocks/auth/authorizedParties/index.js";
+import { getClients } from "./commonFunctions.js";
+import { getItemFromList, getOptions } from "../../../../helpers.js";
+export { setup } from "./commonFunctions.js";
+
+const includeAltinn2 = (__ENV.INCLUDE_ALTINN2 ?? "true") === "true";
+const randomize = (__ENV.RANDOMIZE ?? "true") === "true";
+
+const label = "getAuthorizedPartiesForUserIncludePartiesViaKeyRole";
+
+export const options = getOptions([label]);
+
+export default function (data) {
+    const [authorizedPartiesClient] = getClients();
+    const userParty = getItemFromList(data, randomize);
+    const queryParams = {
+        includeAltinn2: includeAltinn2,
+        includePartiesViaKeyRoles: false
+    };
+    GetAuthorizedParties(
+        authorizedPartiesClient,
+        "urn:altinn:person:identifier-no",
+        userParty.ssn,
+        queryParams,
+        label
+    );
+}
