089c1fc5b1adde1e03fb2f7e78675dea98a8a371
Dagfinn Olsen <dagfinno@gmail.com>
2026-01-09 12:08:11 +0100

    added all tests combinations

---
+++ K6/api/tests/authentication/get-authorized-parties/for-user-many-parties/w-altinn3-w-accesspackages.js
@@ -0,0 +1,26 @@
+
+import { GetAuthorizedParties } from "../../../../building-blocks/authentication/authorized-parties/index.js";
+import { getClients } from "../common-functions.js";
+import { getItemFromList, getOptions } from "../../../../../helpers.js";
+import { endUsers, endUserLabels } from "./end-users.js";
+
+const randomize = (__ENV.RANDOMIZE ?? "false") === "true";
+
+export const options = getOptions(endUserLabels);
+
+export default function () {
+    const [authorizedPartiesClient] = getClients();
+    const userParty = getItemFromList(endUsers, randomize);
+    const queryParams = {
+        includeAltinn3: "true",
+        includeAccessPackages: "true",
+    };
+    
+    GetAuthorizedParties(
+        authorizedPartiesClient,
+        "urn:altinn:person:identifier-no",
+        userParty.pid,
+        queryParams,
+        userParty.label,
+    );
+}
