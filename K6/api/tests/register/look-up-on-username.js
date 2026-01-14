8dedb01f4284287fbbea8e36bc9a2afc18caf140
Vegard Nyeng <vegard.nyeng@gmail.com>
2026-01-14 15:39:19 +0100

    bugfix deployed, test was poorly written

--- K6/api/tests/register/look-up-on-username.js
+++ K6/api/tests/register/look-up-on-username.js
@@ -14,16 +14,16 @@ const csvPath = `../../../testdata/register/register-usernames-${__ENV.ENVIRONME
 const usernames = parseCsvData(open(csvPath));
 
 export default function () {
-    const tokenOpts = new Map();
-    tokenOpts.set("env", __ENV.ENVIRONMENT);
-    tokenOpts.set("ttl", 3600);
+  const tokenOpts = new Map();
+  tokenOpts.set("env", __ENV.ENVIRONMENT);
+  tokenOpts.set("ttl", 3600);
 
-    console.log("Environment: ", __ENV.ENVIRONMENT);
+  console.log("Environment: ", __ENV.ENVIRONMENT);
 
-    const token = new PlatformTokenGenerator(tokenOpts);
-    const registerLookupClient = new RegisterLookupClient(__ENV.BASE_URL, token);
+  const token = new PlatformTokenGenerator(tokenOpts);
+  const registerLookupClient = new RegisterLookupClient(__ENV.BASE_URL, token);
 
-    /**
+  /**
    * This test requires a username that exists in Register:
    * https://github.com/Altinn/altinn-register
    * The username must correspond to a "self identified user" (i.e., a user with email login).
@@ -34,52 +34,55 @@ export default function () {
    * Note: YT01 does not currently have a frontend for user creation.
    * Username should be case insensitive.
    */
-    const user = getItemFromList(usernames, randomize);
+  const user = getItemFromList(usernames, randomize);
 
-    // The user we're trying to lookup
-    const username = user.username;
+  // The user we're trying to lookup
+  const username = user.username;
+  const fields = "person,party,user";
 
-    const fields = "person,party,user";
+  group("Look up username in Register", () => {
+    const requestBody = {
+      data: [`urn:altinn:party:username:${username}`],
+    };
 
-    group("Look up username in Register", () => {
-        const requestBody = {
-            data: [`urn:altinn:party:username:${username}`],
-        };
+    const response = LookupPartiesInRegister(
+      registerLookupClient,
+      fields,
+      requestBody,
+      label
+    );
 
-        const response = LookupPartiesInRegister(
-            registerLookupClient,
-            fields,
-            requestBody,
-            label
-        );
-
-        check(response, {
-            "Username is included in the response 'Vegard'": (r) =>
-                r.body.toLowerCase().includes(username.toLowerCase()),
-            "User is of type self-identified-user": (r) =>
-                r.body.includes("self-identified-user"),
-        });
-
-        group("Look up username in Register - case insensitivity", () => {
-            // Uppercase the username if not already, to test case insensitivity
-            const usernameWithUpperCase = username === username.toUpperCase();
-
-            const requestBody = {
-                data: [`urn:altinn:party:username:${usernameWithUpperCase}`],
-            };
-
-            const response = LookupPartiesInRegister(
-                registerLookupClient,
-                fields,
-                requestBody,
-                label
-            );
-
-            check(response, {
-                "Username with case variant was found in the response": (r) =>
-                    r.body.includes(usernameWithUpperCase),
-            });
-        });
+    check(response, {
+      "Username is included in the response 'Vegard'": (r) =>
+        r.body.toLowerCase().includes(username.toLowerCase()),
+      "User is of type self-identified-user": (r) =>
+        r.body.includes("self-identified-user"),
     });
+
+    group("Look up username in Register - case insensitivity", () => {
+      // Uppercase the username if not already, to test case insensitivity
+      const usernameWithUpperCase = username.toUpperCase();
+
+      console.log("Username with uppercase: ", usernameWithUpperCase);
+
+      const requestBody = {
+        data: [`urn:altinn:party:username:${usernameWithUpperCase}`],
+      };
+
+      const response = LookupPartiesInRegister(
+        registerLookupClient,
+        fields,
+        requestBody,
+        label
+      );
+
+      console.log("Response: ", response.body);
+
+      check(response, {
+        "Username with case variant was found in the response": (r) =>
+          r.body.includes(username),
+      });
+    });
+  });
 }
 export { handleSummary } from "../../../common-imports.js";
