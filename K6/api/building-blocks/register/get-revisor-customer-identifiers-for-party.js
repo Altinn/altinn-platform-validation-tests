43a90c52ffd14b4c1491e2569552ef6d83c73d81
Vegard Nyeng <vegard.nyeng@gmail.com>
2026-01-26 10:17:29 +0100

    linting

--- K6/api/building-blocks/register/get-revisor-customer-identifiers-for-party.js
+++ K6/api/building-blocks/register/get-revisor-customer-identifiers-for-party.js
@@ -9,26 +9,26 @@ import { RegisterApiClient } from "../../../clients/authentication/index.js";
  * @returns {[]}
  */
 export function GetRevisorCustomerIdentifiersForParty(
-  registerClient,
-  facilitatorPartyUuid,
-  subscriptionKey,
-) {
-  const res = registerClient.GetRevisorCustomerIdentifiersForParty(
+    registerClient,
     facilitatorPartyUuid,
     subscriptionKey,
-  );
+) {
+    const res = registerClient.GetRevisorCustomerIdentifiersForParty(
+        facilitatorPartyUuid,
+        subscriptionKey,
+    );
 
-  const success = check(res, {
-    "Register customer list for revisor should respond 200 OK": (r) =>
-      r.status === 200,
-    "Register customer list for revisor response body is not empty": (r) =>
-      r.body !== null && r.body !== undefined,
-  });
-  if (!success) {
-    console.error(res.status);
-    console.error(res.body);
-  }
+    const success = check(res, {
+        "Register customer list for revisor should respond 200 OK": (r) =>
+            r.status === 200,
+        "Register customer list for revisor response body is not empty": (r) =>
+            r.body !== null && r.body !== undefined,
+    });
+    if (!success) {
+        console.error(res.status);
+        console.error(res.body);
+    }
 
-  const body = JSON.parse(res.body);
-  return (body.data ?? []).map((entry) => entry.organizationIdentifier);
+    const body = JSON.parse(res.body);
+    return (body.data ?? []).map((entry) => entry.organizationIdentifier);
 }
