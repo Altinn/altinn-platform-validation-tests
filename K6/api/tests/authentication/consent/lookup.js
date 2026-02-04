a1bfb316fc43c05d6831cd8ea694065710712b66
Vegard Nyeng <vegard.nyeng@gmail.com>
2026-02-04 18:19:25 +0100

    read testdata from main branch

--- K6/api/tests/authentication/consent/lookup.js
+++ K6/api/tests/authentication/consent/lookup.js
@@ -11,9 +11,8 @@ import { LookupConsent } from "../../../building-blocks/authentication/consent/i
 const env = __ENV.ENVIRONMENT ?? "yt01";
 
 export function setup() {
-    // TODO - set to main before merging
     const res = http.get(
-        `https://raw.githubusercontent.com/Altinn/altinn-platform-validation-tests/refs/heads/feature/consent-maskinporten-lookup/K6/testdata/authentication/consent/consentdata-${__ENV.ENVIRONMENT}.csv`
+        `https://raw.githubusercontent.com/Altinn/altinn-platform-validation-tests/refs/heads/main/K6/testdata/authentication/consent/consentdata-${__ENV.ENVIRONMENT}.csv`
     );
     return parseCsvData(res.body);
 }
