d3907f777a3804cf9af2dd66ec3e7a48b0ff505f
Dagfinn Olsen <dagfinno@gmail.com>
2026-03-03 09:22:51 +0100

    added test for clientdelegations my clients (#168)
    * added test for clientdelegations my clients
    
    * added comments
    
    * linting
    
    * fixing test_scope
    
    * regenerate workflow files (#169)
    
    ---------
    
    Co-authored-by: Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>

---
+++ K6/api/tests/authentication/client-delegations/get-my-clients.js
@@ -0,0 +1,66 @@
+import { GetMyClients } from "../../../building-blocks/authentication/client-delegations/index.js";
+import { ClientDelegationsApiClient } from "../../../../clients/authentication/client-delegations.js";
+import { PersonalTokenGenerator } from "../../../../common-imports.js";
+import { getItemFromList, getOptions } from "../../../../helpers.js";
+
+// Labels for different actions
+const tokenGeneratorLabel = "Personal Token Generator";
+
+const environment = __ENV.ENVIRONMENT || "yt01";
+
+// Testdata - fetched from yt01, some systemusers with lots of clients, and some regular users with fewer clients
+// Note that the testdata is not deterministic, and the number of clients for each user may change over time. 
+// Add for other environments as needed, but be aware that the testdata may change over time, 
+// and the test may need to be updated accordingly.
+const endUsersByEnvironment = {
+    yt01: [
+        { uuid: "275d26bf-ad2b-4da6-a872-01828ed2efa1", label: "a_systemuser_78k" },
+        { uuid: "e8b458b6-6fa1-46e2-87fb-103ae1b8c100", label: "b_systemuser_52K" },
+        { uuid: "438ff2ee-4c48-43df-94df-133a13c281ce", label: "c_systemuser_30k" },
+        { uuid: "a364dbe0-b90d-4ee3-85bb-3077c3ec180e", label: "d_systemuser_20k" },
+        { uuid: "7533b7af-9a6a-4bb8-b1ea-8732ec903316", label: "e_systemuser_12k" },
+        { uuid: "bfb5cc27-6249-4a92-874d-dc610366ff41", label: "f_systemuser_10k" },
+        { uuid: "c7c43854-0dc5-4eca-9f3e-b4a8f7d53e61", label: "g_user_1.4k" },
+        { uuid: "5246cca4-699e-4e6a-a21d-0163a95b0371", label: "h_user_1.3k" },
+    ],
+};
+
+const endUsers = endUsersByEnvironment[environment] || [];
+const endUserLabels = [...endUsers.map(user => user.label), tokenGeneratorLabel];
+let tokenGenerator = undefined;
+let clientDelegationsApiClient = undefined;
+
+
+// get k6 options
+export const options = getOptions(endUserLabels);
+
+/**
+ * Main function executed by each VU.
+ */
+export default function () {
+    if (tokenGenerator === undefined) {
+        const tokenOpts = new Map();
+        tokenOpts.set("env", environment);
+        tokenOpts.set("ttl", 3600);
+        tokenOpts.set("scopes", "altinn:portal/enduser");
+        tokenGenerator = new PersonalTokenGenerator(tokenOpts);
+    }
+    if (clientDelegationsApiClient === undefined) {
+        clientDelegationsApiClient = new ClientDelegationsApiClient(__ENV.BASE_URL, tokenGenerator);
+    }
+    const party = getItemFromList(endUsers, false);
+    tokenGenerator.setTokenGeneratorOptions(getTokenOpts(party.uuid));
+    GetMyClients(
+        clientDelegationsApiClient,
+        party.label,
+    );
+}
+
+function getTokenOpts(uuid) {
+    const tokenOpts = new Map();
+    tokenOpts.set("env", environment);
+    tokenOpts.set("ttl", 3600);
+    tokenOpts.set("scopes", "altinn:portal/enduser");
+    tokenOpts.set("partyuuid", uuid);
+    return tokenOpts;
+}
---
+++ K6/clients/authentication/client-delegations.js
@@ -0,0 +1,48 @@
+import http from "k6/http";
+
+class ClientDelegationsApiClient {
+    /**
+     *
+     * @param {string} baseUrl e.g. https://platform.at23.altinn.cloud
+     * @param {*} tokenGenerator
+     */
+    constructor(
+        baseUrl,
+        tokenGenerator,
+    ) {
+        /**
+        * @property {*} tokenGenerator A class that generates tokens used in authenticated calls to the API
+        */
+        this.tokenGenerator = tokenGenerator;
+        /**
+         * @property {string} BASE_PATH The path to the api without host information
+         */
+        this.BASE_PATH = "/accessmanagement/api/v1/enduser/clientdelegations";
+        /**
+         * @property {string} FULL_PATH The path to the api including protocol, hostname, etc.
+         */
+        this.FULL_PATH = baseUrl + this.BASE_PATH;
+    }
+
+    /**
+     * Get clients where the user is agent
+     * Docs: TODO: add link to docs
+     * @param {string|null} label - label for the request
+     * @return http.RefinedResponse
+     */
+    GetMyClients(label = null) {
+        const token = this.tokenGenerator.getToken();
+        const url = new URL(`${this.FULL_PATH}/my/clients`);
+        const tags = label ? label : url.toString();
+        const params = {
+            tags: { name: tags },
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-type": "application/json",
+            },
+        };
+        return http.get(url.toString(), params);
+    }
+}
+
+export { ClientDelegationsApiClient };
