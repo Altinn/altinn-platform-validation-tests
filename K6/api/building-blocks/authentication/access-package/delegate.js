792a60e3555a2068cb06ee7458e34ad6b3082668
Dagfinn Olsen <dagfinno@gmail.com>
2026-03-03 11:38:43 +0100

    linting

--- K6/api/building-blocks/authentication/access-package/delegate.js
+++ K6/api/building-blocks/authentication/access-package/delegate.js
@@ -10,22 +10,22 @@ import { BffAccessPackageApiClient } from "../../../../clients/authentication/in
  * @returns (string | ArrayBuffer | null)
  */
 export function PostDelegations(accessPackageApiClient, queryParams, label = null) {
-  const res = accessPackageApiClient.PostDelegations(queryParams, label);
-  const succeed = check(res, {
-    "PostDelegations - status code is 200": (r) => r.status === 200,
-    "PostDelegations - status text is 200 OK": (r) => r.status_text == "200 OK",
-    "PostDelegations - body is not empty": (r) => {
-      const res_body = JSON.parse(r.body);
-      return res_body !== null && res_body !== undefined;
-    }
-  });
+    const res = accessPackageApiClient.PostDelegations(queryParams, label);
+    const succeed = check(res, {
+        "PostDelegations - status code is 200": (r) => r.status === 200,
+        "PostDelegations - status text is 200 OK": (r) => r.status_text == "200 OK",
+        "PostDelegations - body is not empty": (r) => {
+            const res_body = JSON.parse(r.body);
+            return res_body !== null && res_body !== undefined;
+        }
+    });
 
-  if (!succeed) {
-    console.log(res.status);
-    console.log(res.status_text);
-    console.log(res.body);
-  }
-  return res.body;
+    if (!succeed) {
+        console.log(res.status);
+        console.log(res.status_text);
+        console.log(res.body);
+    }
+    return res.body;
 }
 
 /**
@@ -36,22 +36,22 @@ export function PostDelegations(accessPackageApiClient, queryParams, label = nul
  * @returns (string | ArrayBuffer | null)
  */
 export function DeleteDelegations(accessPackageApiClient, queryParams, label = null) {
-  const res = accessPackageApiClient.DeleteDelegations(queryParams, label);
-  const succeed = check(res, {
-    "DeleteDelegations - status code is 200": (r) => r.status === 200,
-    "DeleteDelegations - status text is 200 OK": (r) => r.status_text == "200 OK",
-    "DeleteDelegations - body is not empty": (r) => {
-      const res_body = JSON.parse(r.body);
-      return res_body !== null && res_body !== undefined;
-    }
-  });
+    const res = accessPackageApiClient.DeleteDelegations(queryParams, label);
+    const succeed = check(res, {
+        "DeleteDelegations - status code is 200": (r) => r.status === 200,
+        "DeleteDelegations - status text is 200 OK": (r) => r.status_text == "200 OK",
+        "DeleteDelegations - body is not empty": (r) => {
+            const res_body = JSON.parse(r.body);
+            return res_body !== null && res_body !== undefined;
+        }
+    });
 
-  if (!succeed) {
-    console.log(res.status);
-    console.log(res.status_text);
-    console.log(res.body);
-  }
-  return res.body;
+    if (!succeed) {
+        console.log(res.status);
+        console.log(res.status_text);
+        console.log(res.body);
+    }
+    return res.body;
 }
 
 /**
@@ -62,22 +62,22 @@ export function DeleteDelegations(accessPackageApiClient, queryParams, label = n
  * @returns (string | ArrayBuffer | null)
  */
 export function GetDelegationCheck(accessPackageApiClient, queryParams, label = null) {
-  const res = accessPackageApiClient.GetDelegationCheck(queryParams, label);
-  const succeed = check(res, {
-    "GetDelegationCheck - status code is 200": (r) => r.status === 200,
-    "GetDelegationCheck - status text is 200 OK": (r) => r.status_text == "200 OK",
-    "GetDelegationCheck - body is not empty": (r) => {
-      const res_body = JSON.parse(r.body);
-      return res_body !== null && res_body !== undefined;
-    }
-  });
+    const res = accessPackageApiClient.GetDelegationCheck(queryParams, label);
+    const succeed = check(res, {
+        "GetDelegationCheck - status code is 200": (r) => r.status === 200,
+        "GetDelegationCheck - status text is 200 OK": (r) => r.status_text == "200 OK",
+        "GetDelegationCheck - body is not empty": (r) => {
+            const res_body = JSON.parse(r.body);
+            return res_body !== null && res_body !== undefined;
+        }
+    });
 
-  if (!succeed) {
-    console.log(res.status);
-    console.log(res.status_text);
-    console.log(res.body);
-  }
-  return res.body;
+    if (!succeed) {
+        console.log(res.status);
+        console.log(res.status_text);
+        console.log(res.body);
+    }
+    return res.body;
 }
 
 /**
@@ -89,20 +89,20 @@ export function GetDelegationCheck(accessPackageApiClient, queryParams, label = 
  * @returns (string | ArrayBuffer | null)
  */
 export function GetPermission(accessPackageApiClient, accessPackageId, queryParams, label = null) {
-  const res = accessPackageApiClient.GetPermission(accessPackageId, queryParams, label);
-  const succeed = check(res, {
-    "GetPermission - status code is 200": (r) => r.status === 200,
-    "GetPermission - status text is 200 OK": (r) => r.status_text == "200 OK",
-    "GetPermission - body is not empty": (r) => {
-      const res_body = JSON.parse(r.body);
-      return res_body !== null && res_body !== undefined;
-    }
-  });
+    const res = accessPackageApiClient.GetPermission(accessPackageId, queryParams, label);
+    const succeed = check(res, {
+        "GetPermission - status code is 200": (r) => r.status === 200,
+        "GetPermission - status text is 200 OK": (r) => r.status_text == "200 OK",
+        "GetPermission - body is not empty": (r) => {
+            const res_body = JSON.parse(r.body);
+            return res_body !== null && res_body !== undefined;
+        }
+    });
 
-  if (!succeed) {
-    console.log(res.status);
-    console.log(res.status_text);
-    console.log(res.body);
-  }
-  return res.body;
+    if (!succeed) {
+        console.log(res.status);
+        console.log(res.status_text);
+        console.log(res.body);
+    }
+    return res.body;
 }
