d394a364f77210059757db78fe5b26733f752156
Dagfinn Olsen <dagfinno@gmail.com>
2026-03-04 16:53:29 +0100

    added file

--- K6/api/building-blocks/authentication/client-delegations/index.js
+++ K6/api/building-blocks/authentication/client-delegations/index.js
@@ -1,3 +1,4 @@
 export { GetMyClients } from "./get-client-delegations.js";
 export { GetAgents, PostAgents, DeleteAgents, GetClients } from "./agents.js";
 export { GetAccessPackages, PostAccessPackages, DeleteAccessPackages } from "./agents-access-packages.js";
+export { PostSingleRight } from "./single-rights.js"; 
