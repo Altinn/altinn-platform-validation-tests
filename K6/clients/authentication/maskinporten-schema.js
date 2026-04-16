f5b874bc77a45da048a72233a9bf170d481fcecd
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-04-16 09:15:02 +0200

    do not add dynamic values to tag values (#221)

--- K6/clients/authentication/maskinporten-schema.js
+++ K6/clients/authentication/maskinporten-schema.js
@@ -77,7 +77,7 @@ class MaskinportenSchemaApiClient {
         Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
         const tags = label ? label : url.toString();
         const params = {
-            tags: { name: tags, endpoint: url.toString() },
+            tags: { name: tags, endpoint: `${this.FULL_PATH}maskinporten/delegations` },
             headers: {
                 Authorization: "Bearer " + token,
                 "Content-type": "application/json",
