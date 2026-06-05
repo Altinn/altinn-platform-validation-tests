a185a5b3f9aeefaa1472b87de3db4d243f87eba0
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-06-05 14:55:50 +0200

    Looks like I can't upload the whole report, there is a size limit

--- playwright/run-playwright-tests.sh
+++ playwright/run-playwright-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export APP_ENV=altinn-access-management-frontend
+
 # git clone https://github.com/Altinn/altinn-platform-validation-tests.git
 # cd altinn-platform-validation-tests/playwright
 git clone https://github.com/monteiro-renato/altinn-access-management-frontend.git
@@ -9,29 +11,30 @@ cd altinn-access-management-frontend/playwright
 npm i --prefix .
 
 # npm run example || true # Needs to be the input
-./node_modules/.bin/playwright test
+./node_modules/.bin/playwright test || true
 
 set +e
 /tmp/generateMetricsFromJunitReport
 exit_code=$?
 set -e
 
-if [ "$exit_code" -eq 53 ]; then
-    echo "Not all Playwright Tests ran successfully, uploading the report..."
-
-    npx -y @azure/static-web-apps-cli deploy \
+#if [ "$exit_code" -eq 53 ]; then
+    #echo "Not all Playwright Tests ran successfully, uploading the report..."
+echo --env "$APP_ENV"
+npx -y @azure/static-web-apps-cli deploy \
         --app-location "./playwright-report" \
         --deployment-token "$APP_TOKEN" \
         --subscription-id "$SUBSCRIPTION_ID" \
         --resource-group playwright-rg \
         --app-name playwright-reports-webapp \
         --swa-config-location /etc/swa-config/ \
-    	--env Production # TODO: Tweak this
+    	--env "$APP_ENV" \
+
 
     #curl \
     #    -s -X POST "$SLACK_WEBHOOK_URL" \
     #    -H 'Content-type: application/json' \
     #    --data "{\"text\":\"Playwright tests failed, report in: $REPORT_URL\"}"
-else
-    echo "All Playwright Tests ran successfully."
-fi
+#else
+#    echo "All Playwright Tests ran successfully."
+#fi
