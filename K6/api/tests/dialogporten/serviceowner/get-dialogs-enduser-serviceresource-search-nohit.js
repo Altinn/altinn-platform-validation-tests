0eb45c8b978bc93e9fa1046f7f852e73a50273fb
Dagfinn Olsen <dagfinno@gmail.com>
2026-06-17 11:30:25 +0200

    added

---
+++ K6/api/tests/dialogporten/serviceowner/get-dialogs-enduser-serviceresource-search-nohit.js
@@ -0,0 +1,28 @@
+import { serviceResources, getClients, texts_no_hit } from "./common-functions.js";
+import { getItemFromList, getOptions } from "../../../../helpers.js";
+import { GetDialogs } from "../../../building-blocks/dialogporten/serviceowner/index.js";
+export { setup } from "./common-functions.js";
+
+const randomize = (__ENV.RANDOMIZE ?? "true") === "true";
+
+const getDialogslabel = { action: "1. get-dialogs-enduser-serviceresource-search-nohit" };
+
+export const options = getOptions([
+    getDialogslabel,
+]);
+
+export default function (data) {
+    const [serviceOwnerApiClient] = getClients();
+    const ssn = getItemFromList(data, randomize).ssn;
+    const resource = getItemFromList(serviceResources, randomize);
+    const queryParams = {
+        endUserId: `urn:altinn:person:identifier-no:${ssn}`,
+        serviceResource: `urn:altinn:resource:${resource}`,
+        Search: getItemFromList(texts_no_hit, true),
+    };
+    GetDialogs(
+        serviceOwnerApiClient,
+        queryParams,
+        getDialogslabel,
+    );
+}
