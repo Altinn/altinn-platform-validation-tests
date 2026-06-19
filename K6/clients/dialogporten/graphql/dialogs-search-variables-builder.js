a6d1007321c56c908e9d936a25547d71b2a5cdde
Dagfinn Olsen <dagfinno@gmail.com>
2026-06-19 12:03:39 +0200

    Added JSdocs

--- K6/clients/dialogporten/graphql/dialogs-search-variables-builder.js
+++ K6/clients/dialogporten/graphql/dialogs-search-variables-builder.js
@@ -1,3 +1,21 @@
+/**
+ * @typedef {Object} DialogSearchVariables
+ * @property {string[]} partyURIs
+ * @property {string=} search
+ * @property {string[]=} org
+ * @property {string[]=} status
+ * @property {string=} continuationToken
+ * @property {number=} limit
+ * @property {string[]=} label
+ * @property {string=} updatedAfter
+ * @property {string=} updatedBefore
+ * @property {string=} searchLanguageCode
+ * @property {string[]=} serviceResources
+ */
+
+/**
+ * Builder for Dialogporten GraphQL search variables.
+ */
 export class DialogSearchVariablesBuilder {
     constructor() {
         this.variables = {
@@ -16,6 +34,11 @@ export class DialogSearchVariablesBuilder {
         };
     }
 
+    /**
+     * Add parties to the search variables. The party ids can be either pid/ssn (11 digits) or org number (9 digits), and the builder will convert them to the expected party URI format.
+     * @param {string []} parties 
+     * @returns 
+     */
     withParties(parties) {
         const partyUris = [];
         for (const party of parties) {
@@ -34,63 +57,122 @@ export class DialogSearchVariablesBuilder {
         return this;
     }
 
+    /**
+     * Add parties to the search variables. The party ids can be either pid/ssn (11 digits) or org number (9 digits), and the builder will convert them to the expected party URI format.
+     * @param {string []} parties. Parties already on the URI format urn:altinn:person:identifier-no:{pid} or urn:altinn:organization:identifier-no:{orgnr}
+     * @returns 
+     */
     withPartyURIs(partyURIs) {
         this.variables.partyURIs = Array.isArray(partyURIs) ? partyURIs : [partyURIs];
         return this;
     }
 
+    /**
+     * Adds a search string to the search variables. The search string can be used to search for dialogs that contain the specified string in the title or content of the dialog.
+     * @param {searchterm} search 
+     * @returns 
+     */
     withSearch(search) {
         this.variables.search = search;
         return this;
     }
 
+    /**
+     * Adds an organization filter to the search variables. The organization filter can be used to search for dialogs that are associated with the specified organization(s). 
+     * The organizations will be the short abbreviation of the organization, e.g. "nav" for the Norwegian Labour and Welfare Administration, "skd" for the Norwegian Tax Administration, etc.
+     * @param {string []} orgs. The short abbreviation of the organization(s) to filter by, e.g. "nav" for the Norwegian Labour and Welfare Administration, "skd" for the Norwegian Tax Administration, etc.
+     * @returns 
+     */
     withOrg(org) {
         this.variables.org = Array.isArray(org) ? org : [org];
         return this;
     }
 
+    /**
+     * Adds a status filter to the search variables. The status filter can be used to search for dialogs that have the specified status(es). Valid statuses are: NOT_APPLICABLE, IN_PROGRESS, AWAITING, REQUIRES_ATTENTION, COMPLETED.
+     * @param {string | string []} status The status or statuses to filter by. Valid statuses are: NOT_APPLICABLE, IN_PROGRESS, AWAITING, REQUIRES_ATTENTION, COMPLETED. 
+     * @returns 
+     */
     withStatus(status) {
         this.variables.status = Array.isArray(status) ? status : [status];
         return this;
     }
 
+    /**
+     * Adds a continuation token to the search variables. The continuation token can be used to paginate through search results. When a search query returns more results than the specified limit, a continuation token is included in the response. This token can be used in a subsequent search query to retrieve the next page of results.
+     * @param {*} token 
+     * @returns 
+     */
     withContinuationToken(token) {
         this.variables.continuationToken = token;
         return this;
     }
 
+    /**
+     * Adds a limit to the search variables. The limit can be used to specify the maximum number of results to return in a search query. If the number of results exceeds the limit, a continuation token will be included in the response that can be used to retrieve the next page of results.
+     * @param { number } limit must be between 1 and 1000. 
+     * @returns 
+     */
     withLimit(limit) {
         this.variables.limit = limit;
         return this;
     }
 
+    /**
+     * @param {string|string[]} label
+     * @returns {DialogSearchVariablesBuilder}
+     */
     withLabel(label) {
-        this.variables.label = Array.isArray(label) ? label : [label];
+        this.variables.label = Array.isArray(label)
+            ? label
+            : [label];
+
         return this;
     }
 
-    withUpdatedAfter(dateTime) {
-        this.variables.updatedAfter = dateTime;
+    /**
+     * @param {string} updatedAfter
+     * @returns {DialogSearchVariablesBuilder}
+     */
+    withUpdatedAfter(updatedAfter) {
+        this.variables.updatedAfter = updatedAfter;
         return this;
     }
 
-    withUpdatedBefore(dateTime) {
-        this.variables.updatedBefore = dateTime;
+    /**
+     * @param {string} updatedBefore
+     * @returns {DialogSearchVariablesBuilder}
+     */
+    withUpdatedBefore(updatedBefore) {
+        this.variables.updatedBefore = updatedBefore;
         return this;
     }
 
-    withSearchLanguageCode(languageCode) {
-        this.variables.searchLanguageCode = languageCode;
+    /**
+     * @param {string} searchLanguageCode
+     * @returns {DialogSearchVariablesBuilder}
+     */
+    withSearchLanguageCode(searchLanguageCode) {
+        this.variables.searchLanguageCode = searchLanguageCode;
         return this;
     }
 
+    /**
+     * @param {string|string[]} serviceResources
+     * @returns {DialogSearchVariablesBuilder}
+     */
     withServiceResources(serviceResources) {
-        this.variables.serviceResources = Array.isArray(serviceResources)
-            ? serviceResources
-            : [serviceResources];
+        this.variables.serviceResources =
+            Array.isArray(serviceResources)
+                ? serviceResources
+                : [serviceResources];
 
         return this;
     }
+
+    /**
+     * @returns {DialogSearchVariables}
+     */
 
     build() {
         return { ...this.variables };
