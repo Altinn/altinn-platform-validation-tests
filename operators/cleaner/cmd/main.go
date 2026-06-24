fda8d4c3da4f642e25e29521f47987400be80397
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-06-24 08:48:26 +0200

    kubebuilder create api --group  --version v1 --kind ConfigMap --controller --resource=false

--- operators/cleaner/cmd/main.go
+++ operators/cleaner/cmd/main.go
@@ -216,6 +216,13 @@ func main() {
 		setupLog.Error(err, "Failed to create controller", "controller", "pod")
 		os.Exit(1)
 	}
+	if err := (&controller.ConfigMapReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "configmap")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
