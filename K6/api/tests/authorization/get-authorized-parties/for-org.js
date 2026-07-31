46f8ebe5467ecf3c54983980a51c47c733413663
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-07-31 13:39:23 +0200

    update paths

--- K6/api/tests/authorization/get-authorized-parties/for-org.js
+++ K6/api/tests/authorization/get-authorized-parties/for-org.js
@@ -1,5 +1,5 @@
-import { AuthorizedPartiesQueryBuilder } from "../../../../clients/authorization/authorized-parties-query.builder.js";
-import { AuthorizedPartiesRequestBuilder } from "../../../../clients/authorization/authorized-parties-request.builder.js";
+import { AuthorizedPartiesQueryBuilder } from "../../../../clients/access-management/resource-owner/authorized-parties/authorized-parties-query.builder.js";
+import { AuthorizedPartiesRequestBuilder } from "../../../../clients/access-management/resource-owner/authorized-parties/authorized-parties-request.builder.js";
 import { getItemFromList, getOptions } from "../../../../helpers.js";
 import { GetAuthorizedParties } from "../../../building-blocks/authorization/authorized-parties/index.js";
 import { getClients } from "./common-functions.js";
--- K6/api/tests/authorization/get-authorized-parties/for-system-user-with-a2.js
+++ K6/api/tests/authorization/get-authorized-parties/for-system-user-with-a2.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 
-import { AuthorizedPartiesQueryBuilder } from "../../../../clients/authorization/authorized-parties-query.builder.js";
-import { AuthorizedPartiesRequestBuilder } from "../../../../clients/authorization/authorized-parties-request.builder.js";
+import { AuthorizedPartiesQueryBuilder } from "../../../../clients/access-management/resource-owner/authorized-parties/authorized-parties-query.builder.js";
+import { AuthorizedPartiesRequestBuilder } from "../../../../clients/access-management/resource-owner/authorized-parties/authorized-parties-request.builder.js";
 import { getItemFromList, getOptions, parseCsvData } from "../../../../helpers.js";
 import { requireEnv } from "../../../../helpers.js";
 import { GetAuthorizedParties } from "../../../building-blocks/authorization/authorized-parties/index.js";
