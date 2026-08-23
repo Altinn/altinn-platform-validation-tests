17a35c030ace589e3a85b5f92a215d80700b8bb4
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-08-23 12:33:22 +0200

    Add extra example env files

---
+++ K6/example_env/prod.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export TOKEN_GENERATOR_USERNAME=""
+export TOKEN_GENERATOR_PASSWORD=""
+
+export BASE_URL="https://platform.altinn.no"
+export ALTINN2_BASE_URL="https://altinn.no"
+export ALTINN_CDN_BASE_URL="https://altinncdn.no"
+export AM_UI_BASE_URL="https://am.ui.altinn.no"
+export AF_UI_BASE_URL="https://af.altinn.no"
+export INFO_CLOUD_URL="https://info.altinn.no"
+export DEPLOY_ENV="prod"
+export ENV_TYPE="prod"
+export ENVIRONMENT="prod"
