a29ef576dee7c54d4f1ca741ffeeeddb36d6a049
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-08-27 09:03:42 +0200

    Add a simple metric to keep track of deleted TestRun resources (#486)

---
+++ operators/cleaner/internal/metrics/metrics.go
@@ -0,0 +1,22 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+var (
+	TestRunDeletionsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "cleaneroperator_testrun_deletions_total",
+			Help: "Total number of TestRun resources deleted by the cleaner operator.",
+		},
+		[]string{"namespace"},
+	)
+)
+
+func init() {
+	metrics.Registry.MustRegister(
+		TestRunDeletionsTotal,
+	)
+}
