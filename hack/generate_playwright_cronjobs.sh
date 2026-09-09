5856bee8b6c59454846a114dd65321a776419c7a
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-09-09 10:11:46 +0200

    Fix PLAYWRIGHT_VERSION setup and fail workflow in case it is null

--- hack/generate_playwright_cronjobs.sh
+++ hack/generate_playwright_cronjobs.sh
@@ -6,7 +6,12 @@ DIST_DIRECTORY=".dist"
 rm -rf "$DIST_DIRECTORY"
 mkdir -p "$DIST_DIRECTORY"
 
-PLAYWRIGHT_VERSION=$(yq -r '."@playwright/test"' package.json)
+PLAYWRIGHT_VERSION=$(yq -r '.devDependencies."@playwright/test"' ./playwright/package.json)
+
+if [[ -z "$PLAYWRIGHT_VERSION" ]]; then
+  echo "Could not find @playwright/test in playwright/package.json" >&2
+  exit 1
+fi
 
 jsonnet --ext-str "playwrightVersion=$PLAYWRIGHT_VERSION" \
         -m "$DIST_DIRECTORY" ./hack/playwright-cronjobs.jsonnet
