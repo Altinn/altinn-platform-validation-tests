b05ce5dbf044950ee6d1e95c3af6b6afb4bf6f29
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2026-09-18 16:31:31 +0200

    Disable Firefox for now

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -86,10 +86,13 @@ export default defineConfig({
     {
       name: 'chromium',
       use: devices['Desktop Chrome'],
-    }, {
+    },
+    /* {
       name: 'firefox',
       use: devices['Desktop Firefox']
-    }, {
+    },
+    */
+    {
       name: 'edge',
       use: devices['Desktop Edge']
     }, {
